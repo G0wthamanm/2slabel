2ec991766ac67076b19f377d7c0141ca6f854ef7
G0wthamanm <103363307+G0wthamanm@users.noreply.github.com>
2024-05-16 10:33:17 +0530

    wsgi

--- macadmin/wsgi.py
+++ macadmin/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hrsuit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'macadmin.settings')
 
 application = get_wsgi_application()
